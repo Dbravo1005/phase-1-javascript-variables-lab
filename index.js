25c7dc2e84e83dccd6d32ddfd906dd3d2a712fe1
Damian Bravo <Dbravo1005@gmail.com>
2023-04-24 22:26:30 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+let companyName = "Scuber";
+
+const companyName2 = 'Duber';
+
+const mostProfitableNeighborhood = "Chelsea";
+
+let mostProfitableNeighborhood2 = "Hell's Kitchen";
+
+const companyCeo = "Susan Smith";
+
+let companyCeo2 = "Jason Smith";
